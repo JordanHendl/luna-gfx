cab7f908ee2a03fef558e4fd48dfb563326b6bc5
Jordan Hendl <jordiehendl@gmail.com>
2022-12-05 00:02:02 -0600

    clean up

---
+++ luna-gfx/gfx.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "luna-gfx/interface/buffer.hpp"
+#include "luna-gfx/interface/image.hpp"
+#include "luna-gfx/interface/render_pass.hpp"
+#include "luna-gfx/interface/window.hpp"
+#include "luna-gfx/interface/pipeline.hpp"
+#include "luna-gfx/interface/bind_group.hpp"
+#include "luna-gfx/interface/command_list.hpp"
--- luna-gfx/interface.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once 
-#include "luna-gfx/interface/buffer.hpp"
-namespace luna {
-namespace gfx {
-auto initialize() -> void;
-}
-}
